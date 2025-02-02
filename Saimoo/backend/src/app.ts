113f16b9fb7a05a17deb7ff1d8de41e46dc62358
The1Re <kanesh132@gmail.com>
2025-02-02 21:08:23 +0700

    add morgan middleware

--- Saimoo/backend/src/app.ts
+++ Saimoo/backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes'
+import routes from './routes';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api', routes);
