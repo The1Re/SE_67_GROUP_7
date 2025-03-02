33d234de63c0cdaf81002912110711cf03ee1883
The1Re <kanesh132@gmail.com>
2025-03-02 23:38:52 +0700

    add map component

--- Saimoo/frontend/src/pages/Trips.tsx
+++ Saimoo/frontend/src/pages/Trips.tsx
@@ -1,10 +1,14 @@
+import MyMap from "@/components/map/MyMap";
 
 export const Trips = () => {
   return (
-    <>
-    <div className="bg-red-500">
-    <div className="mt-16">Trips</div>
+    <div className="h-full w-full flex flex-row">
+      <div className="flex-1/2">
+        <h1 className="mt-16">Trips</h1>
+      </div>
+      <div className="flex-1/2">
+        <MyMap />
+      </div>
     </div>
-    </>
   )
 }
