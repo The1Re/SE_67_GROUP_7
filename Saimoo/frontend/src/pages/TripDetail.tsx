e4f80a8ceb69cfb049ba9e9493e83d047970af31
Aom0114Wasina <oomkoopo@gmail.com>
2025-03-14 20:16:14 +0700

    add tripdetail

---
+++ Saimoo/frontend/src/pages/TripDetail.tsx
@@ -0,0 +1,28 @@
+import MyMap from "@/components/map/MyMap";
+import Image from "@/components/tripdetail/Image";
+import TripCard from "@/components/tripdetail/TripCard";
+import TripDayDetail from "@/components/tripdetail/TripDayDetail";
+
+function TripDetail() {
+ 
+  return (
+    <div className="flex flex-col md:flex-row">
+      {/* คอลัมน์ซ้าย (เนื้อหาหลัก) */}
+      <div className="w-full md:w-4/6 flex flex-col">
+          <Image/>
+          <div className=" w-full bg-white px-8">
+          <TripCard/>
+          <TripDayDetail/>
+          </div>
+
+      </div>
+
+      {/* คอลัมน์ขวา (แผนที่) ✅ ใช้ sticky ทำให้ไม่เลื่อนตาม */}
+      <div className="hidden md:flex w-2/6 h-screen bg-white items-center justify-center sticky top-0">
+          <MyMap />
+      </div>
+    </div>
+  );
+}
+
+export default TripDetail;
